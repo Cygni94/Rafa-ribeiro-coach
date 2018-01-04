0b0869509eec0d9b06c40f8537c3b3d1480cf3aa
Cygni94 <ccoelho.me@gmail.com>
2018-01-03 22:55:22 -0200

    wordpress import

--- wordpress/wp-content/themes/rafaribeiro/footer.php
+++ wordpress/wp-content/themes/rafaribeiro/footer.php
@@ -1,69 +1,68 @@
 <footer class="footer container">
-        <div class="row">
-            <div class="mailing-list__callout offset-md-3 col-md-6 col-sm-12">
-                <span>Entre para nossa lista e receba conteúdos
-                    <br> exclusivos e com prioridade</span>
-            </div>
+    <div class="row">
+        <div class="mailing-list__callout offset-md-3 col-md-6 col-sm-12">
+            <span>Entre para nossa lista e receba conteúdos
+                <br> exclusivos e com prioridade</span>
         </div>
-        <div class="row">
-            <div class="mailing-list__input offset-md-3 col-md-6 col-sm-12">
-                <input type="email" class="mailing-list__input-email" placeholder="Qual o seu  e-mail?">
-                <button class="mailing-list__button" type="submit">Cadastrar</button>
-            </div>
+    </div>
+    <div class="row">
+        <div class="mailing-list__input offset-md-3 col-md-6 col-sm-12">
+            <input type="email" class="mailing-list__input-email" placeholder="Qual o seu  e-mail?">
+            <button class="mailing-list__button" type="submit">Cadastrar</button>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="footer__menu offset-md-2 col-md-5 col-6">
+            <span>
+                <a href="index.html#sobre">sobre</a>
+            </span>
+            <span>
+                <a href="index.html#papo-momento-da-virada">papo momento da virada</a>
+            </span>
+            <span>
+                <a href="index.html#depoimentos">depoimentos</a>
+            </span>
+            <span>contato</span>
+        </div>
+        <div class="footer__social col-md-3 col-6">
+            <span>conecte-se comigo</span>
+            <br>
+            <span>
+                <a href="https://www.facebook.com/rafaribeirocoach/">
+                    <img class="social-icon" src="<?php bloginfo('template_directory'); ?>/img/facebook.png" alt="Facebook">
+                </a>
+            </span>
+            <span>
+                <a href="https://www.instagram.com/rafaribeiro.coach/">
+                    <img class="social-icon" src="<?php bloginfo('template_directory'); ?>/img/instagram.png" alt="Instagram">
+                </a>
+            </span>
+        </div>
+    </div>
+    <div class="footer__bottom row">
+        <div class="footer__logo col-12">
+            <img src="<?php bloginfo('template_directory'); ?>/img/rafa_ribeiro_logo_rodape.png" alt="">
         </div>
         <br>
-        <div class="row">
-            <div class="footer__menu offset-md-2 col-md-5 col-6">
-                <span>
-                    <a href="index.html#sobre">sobre</a>
-                </span>
-                <span>
-                    <a href="index.html#papo-momento-da-virada">papo momento da virada</a>
-                </span>
-                <span>
-                    <a href="index.html#depoimentos">depoimentos</a>
-                </span>
-                <span>contato</span>
-            </div>
-            <div class="footer__social col-md-3 col-6">
-                <span>conecte-se comigo</span>
-                <br>
-                <span>
-                    <a href="https://www.facebook.com/rafaribeirocoach/">
-                        <img class="social-icon" src="assets/img/icon/facebook.png" alt="Facebook">
-                    </a>
-                </span>
-                <span>
-                    <a href="https://www.instagram.com/rafaribeiro.coach/">
-                        <img class="social-icon" src="assets/img/icon/instagram.png" alt="Instagram">
-                    </a>
-                </span>
-            </div>
-        </div>
-        <div class="footer__bottom row">
-            <div class="footer__logo col-12">
-                <img src="assets/img/icon/rafa_ribeiro_logo_rodape.png" alt="">
-            </div>
-            <br>
-            <div class="footer__copyright col-12">
-                <small>© 2017 - Rafa Ribeiro. Todos os direitos reservados. Design orgulhosamente elaborado pelo time Rafa Ribeiro.</small>
-            </div>
-            <br>
-        </div>
+        <div class="footer__copyright col-12">
+            <small>© 2017 - Rafa Ribeiro. Todos os direitos reservados. Design orgulhosamente elaborado pelo time Rafa Ribeiro.</small>
         </div>
         <br>
-    </footer>
+    </div>
+    </div>
+    <br>
+</footer>
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
-        crossorigin="anonymous"></script>
-    <script src="https://use.fontawesome.com/dd1b0d59f0.js"></script>
-    <script src="script.js"></script>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+    crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+    crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/dd1b0d59f0.js"></script>
+<?php wp_footer(); ?>
 </body>
 
 </html>
--- wordpress/wp-content/themes/rafaribeiro/header.php
+++ wordpress/wp-content/themes/rafaribeiro/header.php
@@ -2,12 +2,15 @@
 <html lang="pt-BR">
 
 <head>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<title><?php wp_title(''); ?></title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); wp_head(); ?>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <title>
+        <?php wp_title(''); ?>
+    </title>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' );?>
+    <?php wp_head(); ?>
 </head>
 
 <body>
@@ -15,7 +18,7 @@
         <nav class="nav navbar navbar-dark navbar-expand-lg justify-content-between container">
             <div class="nav__brand">
                 <a class="navbar-brand" href="#">
-                    <img class="nav__brand-logo" src="assets/img/rafa_ribeiro_logo_menu.png" alt="">
+                    <img class="nav__brand-logo" src="<?php bloginfo('template_directory'); ?>/img/rafa_ribeiro_logo_menu.png" alt="">
                 </a>
             </div>
             <button class="navbar-toggler justify-content-end" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
@@ -25,11 +28,11 @@
             <div class="nav__menu collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav">
                     <li class="nav__item nav-item active">
-                        <a class="nav-link" href="index.html#papo-momento-da-virada">papo momento da virada</a>
+                        <a class="nav-link" href="#papo-momento-da-virada">papo momento da virada</a>
                         </a>
                     </li>
                     <li class="nav__item nav-item active">
-                        <a class="nav-link" href="index.html#cursos">treinamentos</a>
+                        <a class="nav-link" href="#cursos">treinamentos</a>
                     </li>
                     <li class="nav__item nav-item active">
                         <a class="nav-link" href="#">contato</a>
