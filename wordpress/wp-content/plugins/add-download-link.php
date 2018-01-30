8a2607d5b457ae3280394809ffe5f116f20bf8da
Cygni94 <ccoelho.me@gmail.com>
2018-01-30 20:26:20 -0200

    ok

---
+++ wordpress/wp-content/plugins/add-download-link.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Add Download Link
+Plugin URI: http://www.damiencarbery.com
+GitHub Plugin URI: https://github.com/damiencarbery/audio-shortcode-download-link
+Description: Add a Download link to the html generated for the "audio" shortcode.
+Author: Damien Carbery
+Version: 0.1
+*/
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+add_filter( 'wp_audio_shortcode', 'ast_audio_shortcode_enhancer', 10, 5 );
+function ast_audio_shortcode_enhancer( $html, $atts, $audio, $post_id, $library ) {
+    $audio_types = array( 'mp3', 'ogg', 'wma', 'm4a', 'wav' );
+    // Use the first audio type that has data.
+    foreach ( $audio_types as $extension ) {
+        if ( strlen( $atts[ $extension ] ) ) {
+            return  $html . sprintf( '<a class="download-btn" href="%s" download></a>', $atts[ $extension ] );
+            break;
+        }
+    }
+    // Otherwise return the original html.
+    return $html;
+}
