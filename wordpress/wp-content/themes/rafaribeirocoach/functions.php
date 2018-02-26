44813427db89d78751529d0efd56208bcf2f94c9
Cygni94 <ccoelho.me@gmail.com>
2018-02-25 21:52:54 -0300

    wp-menu ok

--- wordpress/wp-content/themes/rafaribeirocoach/functions.php
+++ wordpress/wp-content/themes/rafaribeirocoach/functions.php
@@ -170,3 +170,12 @@ function theme_js() {
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_js');
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
