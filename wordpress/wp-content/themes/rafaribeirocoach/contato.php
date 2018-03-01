fae0be84c421ec9cb0b21c36828d1849c3d41b5f
Cygni94 <ccoelho.me@gmail.com>
2018-03-01 20:29:46 -0300

    contato

--- wordpress/wp-content/themes/rafaribeirocoach/contato.php
+++ wordpress/wp-content/themes/rafaribeirocoach/contato.php
@@ -1,6 +1,8 @@
 <?php /* Template Name: Contato */?>
 <?php get_header();?>
 
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/contato.css">
+
 <section class="contact-form">
 
 <h1>Compartilhe conosco suas sugestões, dúvidas e idéias</h1>
