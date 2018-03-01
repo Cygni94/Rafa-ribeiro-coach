f53a74b6a00642e1cbde5da85acd4d70a8a1f149
Cristiano Coelho <ccoelho.me@gmail.com>
2018-03-01 17:18:38 -0300

    desenvolvimento do formulário em andamento

---
+++ wordpress/wp-content/themes/rafaribeirocoach/contato.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Contato */?>
+<?php get_header();?>
+
+<section class="contact-form">
+
+<h1>Compartilhe conosco suas sugestões, dúvidas e idéias</h1>
+
+<?php echo do_shortcode( '[contact-form-7 id="23" title="Formulário de Contato"]' ); ?>
+
+</section>
+
+<?php get_footer();?>
