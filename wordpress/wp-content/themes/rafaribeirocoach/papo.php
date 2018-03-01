05d36896fd91da4ec806ecca28dc349e7018c2f6
Cygni94 <ccoelho.me@gmail.com>
2018-02-28 21:26:38 -0300

    almost done

--- wordpress/wp-content/themes/rafaribeirocoach/papo.php
+++ wordpress/wp-content/themes/rafaribeirocoach/papo.php
@@ -89,7 +89,7 @@
 	<section class="audios">
 		<div class="container">
 			<?php 
-		$args = array( 'post_type' => 'audio', 'posts_per_page' => -1 );	
+		$args = array( 'post_type' => 'audio', 'posts_per_page' => -1 );
 		$loop = new WP_Query( $args );
 		if( $loop->have_posts() ) {?>
 			<div class="audios__list row">
