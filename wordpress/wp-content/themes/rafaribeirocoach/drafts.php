81cc3e2645cedfc633396e93dbf283553c6b9a82
Cygni94 <ccoelho.me@gmail.com>
2018-04-12 00:28:17 -0300

    aperte o play em andamento

--- wordpress/wp-content/themes/rafaribeirocoach/drafts.php
+++ wordpress/wp-content/themes/rafaribeirocoach/drafts.php
@@ -1,95 +1,209 @@
-//INICIO CUSTOMIZER
-    class Depoimento_Customize {
-       public static function register ( $wp_customize ) {
-          //1. Define a new section (if desired) to the Theme Customizer
-          $wp_customize->add_section( 'mytheme_options', 
-             array(
-                'title'       => __( 'MyTheme Options', 'Rafa Ribeiro Coach' ), //Visible title of section
-                'priority'    => 35, //Determines what order this appears in
-                'capability'  => 'edit_theme_options', //Capability needed to tweak
-                'section'     => 'depoimentos',
-                'description' => __('Allows you to customize some example settings for MyTheme.', 'Rafa Ribeiro Coach'), //Descriptive tooltip
-             ) 
-          );
+//INICIO Aperte o play - Benefícios
+function rafaribeiro_beneficios_customizer( $wp_customize ) {
+ 
+    //beneficio section panel
+    $wp_customize->add_panel( 'rafaribeiro_beneficio_options', array(
+            'priority'       => 200,
+            'capability'     => 'edit_theme_options',
+            'title'      => __('Seção beneficios', 'Rafa Ribeiro Coach'),
+        ) );
+        
+        $wp_customize->add_section( 'beneficio_section_head' , array(
+            'title'      => __('Título da seção','Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 50,
+           ) );
+        
+        //Hide Index beneficio Section	
+        $wp_customize->add_setting(
+        'beneficio_options[beneficio_section_enabled]',
+        array(
+            'default' => '',
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'sanitize_text_field',
+            'type' => 'option'
+        )	
+        );
+        $wp_customize->add_control(
+        'beneficio_options[beneficio_section_enabled]',
+        array(
+            'label' => __('Ocultar esta seção','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_section_head',
+            'type' => 'checkbox',
+        )
+        );
 
-          //2. Register new settings to the WP database...
-          $wp_customize->add_setting( 'depoimento_one_image', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
-             array(
-                'default'    => '#EF4136', //Default setting/value to save
-                'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
-                'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
-                'transport'  => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
-             ) 
-          );      
+        $wp_customize->add_setting(
+        'beneficio_options[beneficio_label]',
+        array(
+            'default' => __('beneficios','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' => 'option'
+        )	
+        );
+        $wp_customize->add_control(
+        'beneficio_options[beneficio_label]',
+        array(
+            'label' => __('Title','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_section_head',
+            'type' => 'text',
+        )
+        );
 
-          //3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
-          $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
-             $wp_customize, //Pass the $wp_customize object (required)
-             'rafaribeirocoach_depoimento_background-1', //Set a unique ID for the control
-             array(
-                'label'      => __( 'Imagem do depoimento 1', 'Rafa Ribeiro Coach' ), //Admin-visible name of the control
-                'settings'   => 'depoimento_one_image', //Which setting to load and manipulate (serialized is okay)
-                'priority'   => 10, //Determines the order this control appears in for the specified section
-                'section'    => 'depoimento_section_one', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
-             ) 
-          ) );
+    //beneficio section one
+        $wp_customize->add_section( 'beneficio_item_one' , array(
+            'title'      => __('beneficio 1', 'Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 100,
+            'sanitize_callback' => 'sanitize_text_field',
+           ) );
 
-          //depoimento 2
-          $wp_customize->add_setting( 'depoimento_background-2', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
-             array(
-                'default'    => '#FFD200', //Default setting/value to save
-                'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
-                'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
-                'transport'  => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
-             ) 
-          );      
+        $wp_customize->add_setting(
+        'beneficio_options[beneficio_one_label]',
+        array(
+            'default' => __('você ame e acredite.','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' => 'option'
+        )	
+        );
+        $wp_customize->add_control(
+        'beneficio_options[beneficio_one_label]',
+        array(
+            'label' => __('Legenda','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_item_one',
+            'type' => 'text',
+        )
+        );
+        $wp_customize->add_setting( 'beneficio_options[beneficio_one_image]',
+            array(
+                'default' => get_template_directory_uri().'/assets/img/beneficios/beneficio_xande_bastos.png',
+	            'type' => 'option','sanitize_callback' => 'esc_url_raw',
+            )
+        );
+ 
+	    $wp_customize->add_control(
+		new WP_Customize_Image_Control(
+			$wp_customize,
+			'beneficio_options[beneficio_one_image]',
+			array(
+				'type'        => 'upload',
+				'label' => __('Imagem do beneficio 1','Rafa Ribeiro Coach'),
+				'section' => 'beneficio_item_one',
+				'settings' =>'beneficio_options[beneficio_one_image]',
+			)
+		)
+	);
+        
+    //Second beneficio
+    
+    $wp_customize->add_section( 'beneficio_item_two' , array(
+            'title'      => __('beneficio 2', 'Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 200,
+           ) );
 
-          $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
-             $wp_customize, //Pass the $wp_customize object (required)
-             'rafaribeirocoach_depoimento_background-2', //Set a unique ID for the control
-             array(
-                'label'      => __( 'Imagem do depoimento 2', 'Rafa Ribeiro Coach' ), //Admin-visible name of the control
-                'settings'   => 'depoimento_background-2', //Which setting to load and manipulate (serialized is okay)
-                'priority'   => 11, //Determines the order this control appears in for the specified section
-                'section'    => 'depoimento_section_two', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
-             ) 
-          ) );
+    $wp_customize->add_setting(
+        'beneficio_options[beneficio_two_label]',
+        array(
+            'default' => __('ÁGATHA TOMMASI','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' => 'option',
+        )	
+    );
+    $wp_customize->add_control(
+        'beneficio_options[beneficio_two_label]',
+        array(
+            'label' => __('Legenda' ,'Rafa Ribeiro Coach'),
+            'section' => 'beneficio_item_two',
+            'type' => 'text',
+        )
+    );
+    $wp_customize->add_setting( 'beneficio_options[beneficio_two_image]',
+    array(
+        'default' => get_template_directory_uri().'/assets/img/beneficios/beneficio_agatha.png',
+        'type' => 'option','sanitize_callback' => 'esc_url_raw',
+    )
+);
 
-          //depoimento 3
-          $wp_customize->add_setting( 'depoimento_background-3', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
-             array(
-                'default'    => '#01CAFF', //Default setting/value to save
-                'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
-                'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
-                'transport'  => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
-             ) 
-          );      
-          $wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
-             $wp_customize, //Pass the $wp_customize object (required)
-             'rafaribeirocoach_depoimento_background-3', //Set a unique ID for the control
-             array(
-                'label'      => __( 'Imagem do depoimento 3', 'Rafa Ribeiro Coach' ), //Admin-visible name of the control
-                'settings'   => 'depoimento_background-3', //Which setting to load and manipulate (serialized is okay)
-                'priority'   => 12, //Determines the order this control appears in for the specified section
-                'section'    => 'depoimento_section_three', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
-             ) 
-          ) );
-          
-          //4. We can also change built-in settings by modifying properties. For instance, let's make some stuff use live preview JS...
-          $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
-          $wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
-          $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-          $wp_customize->get_setting( 'background_color' )->transport = 'postMessage';
-       }
+$wp_customize->add_control(
+new WP_Customize_Image_Control(
+    $wp_customize,
+    'beneficio_options[beneficio_two_image]',
+    array(
+        'type'        => 'upload',
+        'label' => __('Imagem do beneficio 2','Rafa Ribeiro Coach'),
+        'section' => 'beneficio_item_two',
+        'settings' =>'beneficio_options[beneficio_two_image]',
+    )
+)
+);
 
-       public static function header_output() {
-          ?>
-          <!--Customizer CSS--> 
-          <style type="text/css">
-               <?php self::generate_css('.depoimentos__item-1', 'background-image', 'depoimento_background-1'); ?>
-               <?php self::generate_css('.depoimentos__item-2', 'background-image', 'depoimento_background-2'); ?>
-               <?php self::generate_css('.depoimentos__item-3', 'background-image', 'depoimento_background-3'); ?>
-          </style> 
-          <!--/Customizer CSS-->
-          <?php
-       }
+    //Third beneficio section
+    $wp_customize->add_section( 'beneficio_item_three' , array(
+            'title'      => __('beneficio 3', 'Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 300,
+           ) );
+
+    $wp_customize->add_setting(
+        'beneficio_options[beneficio_three_label]',
+        array(
+            'default' => __('RAFAEL, DABÁ','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' =>'option',
+        )	
+    );
+    $wp_customize->add_control(
+        'beneficio_options[beneficio_three_label]',
+        array(
+            'label' => __('Autor do beneficio','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_item_three',
+            'type' => 'text',
+        )
+    );
+
+    $wp_customize->add_setting( 'beneficio_options[beneficio_three_image]',
+    array(
+        'default' => get_template_directory_uri().'/assets/img/beneficios/beneficio_rafael.png',
+        'type' => 'option','sanitize_callback' => 'esc_url_raw',
+    )
+);
+
+$wp_customize->add_control(
+new WP_Customize_Image_Control(
+    $wp_customize,
+    'beneficio_options[beneficio_three_image]',
+    array(
+        'type'        => 'upload',
+        'label' => __('Imagem do beneficio 3','Rafa Ribeiro Coach'),
+        'section' => 'beneficio_item_three',
+        'settings' =>'beneficio_options[beneficio_three_image]',
+    )
+)
+);
+
+
+    function rafaribeiro_beneficios_sanitize_html( $input ) {
+        return force_balance_tags( $input );
+        }
+    }
+    add_action( 'customize_register', 'rafaribeiro_beneficios_customizer' );
+
+    function beneficios_setup_data()
+          {
+        return $beneficios_options=array(
+        //beneficio section settings
+        'beneficio_section_enabled' => '',
+        'beneficio_label' => __('beneficios','Rafa Ribeiro Coach'),
+        'beneficio_one_label'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_one_image'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_two_label'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_two_image'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_three_label'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_three_image'=>__('','Rafa Ribeiro Coach'),
+    );
+    }
--- wordpress/wp-content/themes/rafaribeirocoach/functions.php
+++ wordpress/wp-content/themes/rafaribeirocoach/functions.php
@@ -850,3 +850,213 @@ new WP_Customize_Image_Control(
         'depoimento_three_image'=>__('','Rafa Ribeiro Coach'),
     );
     }
+
+//INICIO SEÇÃO BENEFICIOS
+//INICIO Aperte o play - Benefícios
+function rafaribeiro_beneficios_customizer( $wp_customize ) {
+ 
+    //beneficio section panel
+    $wp_customize->add_panel( 'rafaribeiro_beneficio_options', array(
+            'priority'       => 200,
+            'capability'     => 'edit_theme_options',
+            'title'      => __('Seção beneficios', 'Rafa Ribeiro Coach'),
+        ) );
+        
+        $wp_customize->add_section( 'beneficio_section_head' , array(
+            'title'      => __('Título da seção','Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 50,
+           ) );
+        
+        //Hide Index beneficio Section	
+        $wp_customize->add_setting(
+        'beneficio_options[beneficio_section_enabled]',
+        array(
+            'default' => '',
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'sanitize_text_field',
+            'type' => 'option'
+        )	
+        );
+        $wp_customize->add_control(
+        'beneficio_options[beneficio_section_enabled]',
+        array(
+            'label' => __('Ocultar esta seção','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_section_head',
+            'type' => 'checkbox',
+        )
+        );
+
+        $wp_customize->add_setting(
+        'beneficio_options[beneficio_label]',
+        array(
+            'default' => __('beneficios','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' => 'option'
+        )	
+        );
+        $wp_customize->add_control(
+        'beneficio_options[beneficio_label]',
+        array(
+            'label' => __('Title','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_section_head',
+            'type' => 'text',
+        )
+        );
+
+    //beneficio section one
+        $wp_customize->add_section( 'beneficio_item_one' , array(
+            'title'      => __('beneficio 1', 'Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 100,
+            'sanitize_callback' => 'sanitize_text_field',
+           ) );
+           $wp_customize->add_setting( 'beneficio_options[beneficio_one_image]',
+           array(
+               'default' => get_template_directory_uri().'/assets/img/beneficios/beneficio_xande_bastos.png',
+               'type' => 'option','sanitize_callback' => 'esc_url_raw',
+           )
+       );
+
+       $wp_customize->add_control(
+       new WP_Customize_Image_Control(
+           $wp_customize,
+           'beneficio_options[beneficio_one_image]',
+           array(
+               'type'        => 'upload',
+               'label' => __('Imagem do beneficio 1','Rafa Ribeiro Coach'),
+               'section' => 'beneficio_item_one',
+               'settings' =>'beneficio_options[beneficio_one_image]',
+           )
+       )
+   );
+        $wp_customize->add_setting(
+        'beneficio_options[beneficio_one_label]',
+        array(
+            'default' => __('você ame e acredite.','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' => 'option'
+        )	
+        );
+        $wp_customize->add_control(
+        'beneficio_options[beneficio_one_label]',
+        array(
+            'label' => __('Legenda','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_item_one',
+            'type' => 'text',
+        )
+        );
+        
+    //Second beneficio
+    
+    $wp_customize->add_section( 'beneficio_item_two' , array(
+            'title'      => __('beneficio 2', 'Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 200,
+           ) );
+
+           $wp_customize->add_setting( 'beneficio_options[beneficio_two_image]',
+           array(
+               'default' => get_template_directory_uri().'/assets/img/beneficios/beneficio_agatha.png',
+               'type' => 'option','sanitize_callback' => 'esc_url_raw',
+           )
+       );
+       
+       $wp_customize->add_control(
+       new WP_Customize_Image_Control(
+           $wp_customize,
+           'beneficio_options[beneficio_two_image]',
+           array(
+               'type'        => 'upload',
+               'label' => __('Imagem do beneficio 2','Rafa Ribeiro Coach'),
+               'section' => 'beneficio_item_two',
+               'settings' =>'beneficio_options[beneficio_two_image]',
+           )
+       )
+       );
+
+    $wp_customize->add_setting(
+        'beneficio_options[beneficio_two_label]',
+        array(
+            'default' => __('você utilize seus talentos.','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' => 'option',
+        )	
+    );
+    $wp_customize->add_control(
+        'beneficio_options[beneficio_two_label]',
+        array(
+            'label' => __('Legenda' ,'Rafa Ribeiro Coach'),
+            'section' => 'beneficio_item_two',
+            'type' => 'text',
+        )
+    );
+
+    //Third beneficio section
+    $wp_customize->add_section( 'beneficio_item_three' , array(
+            'title'      => __('beneficio 3', 'Rafa Ribeiro Coach'),
+            'panel'  => 'rafaribeiro_beneficio_options',
+            'priority'   => 300,
+           ) );
+
+           $wp_customize->add_setting( 'beneficio_options[beneficio_three_image]',
+           array(
+               'default' => get_template_directory_uri().'/assets/img/beneficios/beneficio_rafael.png',
+               'type' => 'option','sanitize_callback' => 'esc_url_raw',
+           )
+       );
+       
+       $wp_customize->add_control(
+       new WP_Customize_Image_Control(
+           $wp_customize,
+           'beneficio_options[beneficio_three_image]',
+           array(
+               'type'        => 'upload',
+               'label' => __('Imagem do beneficio 3','Rafa Ribeiro Coach'),
+               'section' => 'beneficio_item_three',
+               'settings' =>'beneficio_options[beneficio_three_image]',
+           )
+       )
+       );
+
+           $wp_customize->add_setting(
+        'beneficio_options[beneficio_three_label]',
+        array(
+            'default' => __('traga mais tempo para você e sua família','Rafa Ribeiro Coach'),
+            'capability'     => 'edit_theme_options',
+            'sanitize_callback' => 'rafaribeiro_beneficios_sanitize_html',
+            'type' =>'option',
+        )	
+    );
+    $wp_customize->add_control(
+        'beneficio_options[beneficio_three_label]',
+        array(
+            'label' => __('Legenda','Rafa Ribeiro Coach'),
+            'section' => 'beneficio_item_three',
+            'type' => 'text',
+        )
+    );
+
+    function rafaribeiro_beneficios_sanitize_html( $input ) {
+        return force_balance_tags( $input );
+        }
+    }
+    add_action( 'customize_register', 'rafaribeiro_beneficios_customizer' );
+
+    function beneficios_setup_data()
+          {
+        return $beneficios_options=array(
+        //beneficio section settings
+        'beneficio_section_enabled' => '',
+        'beneficio_label' => __('beneficios','Rafa Ribeiro Coach'),
+        'beneficio_one_label'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_one_image'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_two_label'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_two_image'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_three_label'=>__('','Rafa Ribeiro Coach'),
+        'beneficio_three_image'=>__('','Rafa Ribeiro Coach'),
+    );
+    }
